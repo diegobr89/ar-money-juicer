d8e865071cc416334c6b493c5efa76cbbf1ab099
diegobr89 <42051216+diegobr89@users.noreply.github.com>
2023-01-06 20:37:00 -0300

    Change to read cache only offline

--- sw-v2.js
+++ sw-v2.js
@@ -23,9 +23,22 @@ self.addEventListener('activate', function(event) {
 
 // Manejo de peticiones
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  if (!navigator.onLine) {
+    // Si no hay conexión a internet, devuelve el recurso de la caché
+    event.respondWith(caches.match(event.request));
+  } else {
+    // Si hay conexión a internet, realiza la solicitud a través de internet
+    event.respondWith(
+      fetch(event.request).then(function(response) {
+        // Almacena el recurso en la caché para futuras solicitudes sin conexión a internet
+        caches.open('v1').then(function(cache) {
+          cache.put(event.request, response);
+        });
+        return response.clone();
+      }).catch(function() {
+        // Si la solicitud a través de internet falla, devuelve el recurso de la caché
+        return caches.match(event.request);
+      })
+    );
+  }
 });
