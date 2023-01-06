a0f9638a2656cbcef76caa5bf4c550ff2fab6113
diegobr89 <42051216+diegobr89@users.noreply.github.com>
2023-01-06 18:00:37 -0300

    Updated prices and pwa offline system

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,59 +1,31 @@
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-const CACHE_LIST = [
-  "index.html",
-  "/assets/ar-money-juicer.png",
-  "price-calc-core.js",
-]
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
+// Instalación del Service Worker
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => {
-        CACHE_LIST.forEach(item => cache.add(item));
-      })
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/ar-money-juicer.png',
+        '/css/bootstrap.min.css',
+        '/css/all.min.css',
+        '/css/mdb.min.css',
+        '/js/bootstrap.min.js',
+        '/js/mdb.min.js'
+      ]);
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+// Activa el Service Worker
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activado.');
+});
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-
-        for (const item of CACHE_LIST) {
-          const match = await cache.match(item);
-
-          if(!!match)
-            return match;
-        }
-      }
-    })());
-  }
+// Manejo de peticiones
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
